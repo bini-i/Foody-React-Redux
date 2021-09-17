72c3c6e7cce0f800558777676155c36cf253bb6f
Binyam <binyam@pop-os.localdomain>
2021-09-17 13:51:35 +0300

    Update App.js
    - renders header with Home, category and contact links
    - renders MealsListContainer which is connected to a
      redux store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import './App.css';
+import './styles/global.css';
+import {
+  BrowserRouter,
+  Link,
+} from 'react-router-dom';
+import * as styles from './styles/App.module.css';
+import MealsListContainer from './containers/MealsListContainer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Puppy wear
+    <>
+      <header className={styles.header}>
+        <div className={styles.headerBody}>
+          <h1><a href="/">FOODY</a></h1>
+          <ul>
+            <BrowserRouter>
+              <Link to="/">
+                <li>HOME</li>
+              </Link>
+            </BrowserRouter>
+            <li>CATEGORIES</li>
+            <li>CONTACT</li>
+          </ul>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fillOpacity="1" d="M0,64L120,85.3C240,107,480,149,720,149.3C960,149,1200,107,1320,85.3L1440,64L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z" /></svg>
       </header>
-    </div>
+      <MealsListContainer />
+    </>
   );
 }
 
