9356f99706838b651287c4f6419d9df95df28d45
Binyam <binyam@pop-os.localdomain>
2021-09-17 14:11:16 +0300

    Add src/containers/MealsListContainer.js
    - MealsListContainer container component that is connected to the
      redux store and gets states and action dispatchers as props
    - renders MealsList or Meal component based on the route choosen
    - uses BrowserRouter to allow switching between the MealsList and
      Meal components based on the route taken
    - also displays the list of categories fetched from meals API
      and allows the change the filter state of the store onClick,
      which result in change of meals listed

---
+++ src/containers/MealsListContainer.js
@@ -0,0 +1,56 @@
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { mapDispatchToProps, mapStateToProps } from '../reducers';
+import MealsList from '../components/MealsList';
+import Meal from '../components/Meal';
+import * as styles from '../styles/MealsList.module.css';
+
+const MealsListContainer = ({
+  meals, filter, categories, changeFilter,
+}) => {
+  const changeCategory = (event) => {
+    changeFilter(event.target.dataset.category);
+  };
+
+  const categoryList = categories.map((category) => <li key={category.idCategory}><button type="button" data-category={category.strCategory} onClick={changeCategory}>{category.strCategory}</button></li>);
+
+  return (
+    <div className={styles.mealsBodyWrapper}>
+      <div className={styles.mealsBody}>
+        <section className={styles.mealsSection}>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/">
+                <h2 className={styles.mealsHeader}>{filter}</h2>
+                <div className={styles.mealsList}>
+                  <MealsList meals={meals} filter={filter} />
+                </div>
+              </Route>
+              <Route exact path="/meal">
+                <Meal />
+              </Route>
+            </Switch>
+          </BrowserRouter>
+        </section>
+        <section className={styles.mealsCategory}>
+          <h2 className={styles.categoryHeader}>CATEGORIES</h2>
+          <div className={styles.categoryList}>
+            <ul>
+              {categoryList}
+            </ul>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MealsListContainer);
+
+MealsListContainer.propTypes = {
+  meals: PropTypes.instanceOf(Array).isRequired,
+  categories: PropTypes.instanceOf(Array).isRequired,
+  filter: PropTypes.string.isRequired,
+  changeFilter: PropTypes.func.isRequired,
+};
